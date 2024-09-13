a95ab9d000eee01ef14ec9960af40bfea2ac4a3e
Franco Aparicio <francomaty07@gmail.com>
2024-09-13 12:00:06 -0300

    Crear constructor silla

--- src/Clase01/Main.java
+++ src/Clase01/Main.java
@@ -3,11 +3,8 @@ package Clase01;
 public class Main {
 
 	public static void main(String[] args) {
-		if (true) {
-			
-		} else {
-
-		}
+		
+		Silla silla1 = new Silla("Silla clasica");
 
 	}
 
---
+++ src/Clase01/Silla.java
@@ -0,0 +1,12 @@
+package Clase01;
+
+public class Silla {
+
+	private String nombre;
+
+	public Silla(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+}
